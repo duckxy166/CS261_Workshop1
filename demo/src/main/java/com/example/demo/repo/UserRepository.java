64ceec5de8439264a97518da7c63d47f4b57e56e
saharat <saharat.udo@dome.tu.ac.th>
2025-09-09 16:25:00 +0700

    Change pom

---
+++ demo/src/main/java/com/example/demo/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repo;
+import com.example.demo.model.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+List<User> findByFirstName(String firtsName);
+List<User> findByEmailContaining(String keyword);
+}
